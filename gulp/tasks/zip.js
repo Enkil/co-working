dcffbb432d1d386ad368683a51037d2b9bc1635f
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-05-12 05:48:14 +0300

    news block

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -1,10 +1,10 @@
 const
-    gulp = require('gulp'),
+    gulp =          require('gulp'),
     config =        require('../config').paths,
-    zip = require('gulp-zip');
+    zip =           require('gulp-zip');
 
 gulp.task('zip', () => {
     return gulp.src(config.dist)
         .pipe(zip('archive.zip'))
-        .pipe(gulp.dest(config.root));
+        .pipe(gulp.dest("./"));
 });
